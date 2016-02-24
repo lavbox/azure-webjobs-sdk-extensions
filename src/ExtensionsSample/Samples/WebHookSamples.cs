c84b5d9f7b60bd52100f085e5e9f3926508b036d
LAVG-LAPTOP\lavgupta <gupta.lav@gmail.com>
2016-02-24 22:01:04 +0530

    Only one Hook

--- src/ExtensionsSample/Samples/WebHookSamples.cs
+++ src/ExtensionsSample/Samples/WebHookSamples.cs
@@ -15,80 +15,16 @@ namespace ExtensionsSample
     public static class WebHookSamples
     {
         /// <summary>
-        /// This WebHook uses the default convention based routing, and is invoked
-        /// by POST requests to http://localhost:{port}/WebHookSamples/HookA.
-        /// </summary>
-        public static void HookA([WebHookTrigger] string body, TraceWriter trace)
-        {
-            trace.Info(string.Format("HookA invoked! Body: {0}", body));
-        }
-
-        /// <summary>
-        /// This WebHook declares its route, and is invoked by POST requests
-        /// to http://localhost:{port}/Sample/HookB.
-        /// </summary>
-        public static async Task HookB(
-            [WebHookTrigger("Sample/HookB")] HttpRequestMessage request, 
-            TraceWriter trace)
-        {
-            string body = await request.Content.ReadAsStringAsync();
-            trace.Info(string.Format("HookB invoked! Body: {0}", body));
-        }
-
-        /// <summary>
-        /// Demonstrates binding to a custom POCO Type, including model binding in other
-        /// binders to values from that POCO.
-        /// </summary>
-        public static void HookC(
-            [WebHookTrigger] Order order,
-            [File(@"{OrderId}_{CustomerName}.txt", FileAccess.Write)] out string output,
-            TraceWriter trace)
-        {
-            output = "Order Received!";
-            trace.Info(string.Format("HookC invoked! OrderId: {0}", order.OrderId));
-        }
-
-        /// <summary>
         /// Demonstrates binding to <see cref="WebHookContext"/> which enables you to
         /// control the response returned.
         /// </summary>
         public static void HookD([WebHookTrigger] WebHookContext context)
         {
             var obj = context.Request.Content.ReadAsAsync<JObject>().Result;
-            
-            context.Response = new HttpResponseMessage(HttpStatusCode.Accepted)
-            {
-                Content = new StringContent(string.Format("{{  ToUpper: {0}, OnDate:{1}  }}", ((string)obj["Id"]).ToUpper(CultureInfo.CurrentCulture), System.DateTime.UtcNow.ToString()))
-            };
-        }
-
-        //public static void HookE([WebHookTrigger] WebHookContext context)
-        //{
-        //    var obj = context.Request.Content.ReadAsAsync<JObject>();
-
-        //    context.Response = new HttpResponseMessage(HttpStatusCode.Accepted)
-        //    {
-        //        Content = new StringContent(string.Format("{{  ToUpper: {0}, OnDate:{1}  }}", ((string)obj["Id"]).ToUpper(CultureInfo.CurrentCulture), System.DateTime.UtcNow.ToString()))
-        //    };
-        //}
-
-        /// <summary>
-        /// GitHub WebHook example, showing integration with the ASP.NET WebHooks SDK.
-        /// The route uses the format {receiver}/{id} where the receiver "github" corresponds
-        /// to the <see cref="GitHubWebHookReceiver"/> that was registered on startup via
-        /// <see cref="WebHooksConfiguration.UseReceiver{T}"/>, and the "issues" Id component
-        /// corresponds to the "Issues" secret configured in app.config.
-        /// Incoming requests will be authenticated by <see cref="GitHubWebHookReceiver"/> prior
-        /// to invoking this job function.
-        /// </summary>
-        public static void GitHub(
-            [WebHookTrigger("github/issues")] string body,
-            TraceWriter trace)
-        {
-            dynamic issueEvent = JObject.Parse(body);
-
-            trace.Info(string.Format("GitHub Issues WebHook invoked - Issue: '{0}', Action: '{1}', ",
-                issueEvent.issue.title, issueEvent.action));
+context.Response = new HttpResponseMessage(HttpStatusCode.Accepted)
+{
+    Content = new StringContent(string.Format("{{  CToUpper: {0}, OnDate:{1}  }}", ((string)obj["Id"]).ToUpper(CultureInfo.CurrentCulture), System.DateTime.UtcNow.ToString()))
+};
         }
     }
 }
